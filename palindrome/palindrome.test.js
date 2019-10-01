55fe590479e383b861b34770707882b87d2d73e8
quadriphobs1 <abiodunquadriadekunle@gmail.com>
2019-10-01 13:44:07 +0100

    re-write palindrome algorithmn

---
+++ palindrome/palindrome.test.js
@@ -0,0 +1,22 @@
+const isPalindrome = require('./palindrome');
+
+describe('isPalindrome()', () => {
+  it('should return true for single number', () => {
+    expect(isPalindrome(1)).toBeTruthy();
+  })
+
+  it('should return true for valid number "121"', () => {
+    expect(isPalindrome(121)).toBeTruthy();
+  })
+
+  it('should return true for same number 111', () => {
+    expect(isPalindrome(111)).toBeTruthy();
+  })
+  it('should return false for invalid number', () => {
+    expect(isPalindrome(123)).toBeFalsy();
+  })
+
+  it('should return true for long value', () => {
+    expect(isPalindrome(123321123321)).toBeTruthy();
+  })
+})
