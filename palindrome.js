a405c652a2badf9fe4602e772036add24469e250
raffleberry <raffleberry42@gmail.com>
2019-10-01 18:54:26 +0530

    palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,26 +1,13 @@
-<script>
-	function is_palindrome(number)
-	{
-		var rem, temp, final = 0;
-
-		temp = number;
-		while(number>0)
-		{
-			rem = number%10;
-			number = parseInt(number/10);
-			final = final*10+rem;
-		}
-		if(final==temp)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-  
-  
-  console.log(is_palindrome(121));  //TRUE
-  console.log(is_palindrome(123));  //False
-</script>
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    x = x.toString();
+    var n = x.length;
+    for (var i = 0; i < n/2; i++) {
+        if (x[i] !== x[n-i-1])
+            return false;
+    }
+    return true;
+};
