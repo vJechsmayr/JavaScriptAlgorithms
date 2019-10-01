7bdd7bb8e32497dd62e51368cd057f49a91acf8d
Eduardo Gomes <eduardoqgomes@gmail.com>
2019-10-01 17:01:36 -0300

    Another way to convert decimal to octal

--- NumberConvertions/DecimalToOctal.js
+++ NumberConvertions/DecimalToOctal.js
@@ -3,3 +3,7 @@ function decimalToOctal(num) {
 }
 
 console.log(decimalToOctal(9));
+
+const decimalToOctalExpression = (number) => (number.toString(8));	
+
+console.log(decimalToOctalExpression(9));
