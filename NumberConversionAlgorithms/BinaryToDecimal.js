19b09f4cb369bf7ec3ee471714195b0f636f9bd6
vome_developer <jeyanth.raju@purviewservices.com>
2019-10-02 08:03:57 +0530

    Added BinaryToDecimal, DecimalToHexadecimal, Hexadecimal to Binary Algorithms

---
+++ NumberConversionAlgorithms/BinaryToDecimal.js
@@ -0,0 +1,5 @@
+function binToDec(value) {
+  return parseInt(value, 2);
+}
+
+console.log(binToDec("000110"));
---
+++ NumberConversionAlgorithms/DecimalToHexadecimal.js
@@ -0,0 +1,5 @@
+function decToHex(value) {
+  return value.toString(16);
+}
+
+console.log(decToHex(76876));
