70bd21e649ddd43751fdc8b23bb8ff1ef4f7f715
Aviral kushwaha <34898366+kushavi2509@users.noreply.github.com>
2019-10-05 01:34:00 +0530

    fibonacci seriez

---
+++ Maths/fibonacci.js
@@ -0,0 +1,16 @@
+var fibonacci = function (n) 
+{
+  if (n===1) 
+  {
+    return [0, 1];
+  } 
+  else 
+  {
+    var s = fibonacci(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+
+var result = fibonacci(10)
+console.log(result)
